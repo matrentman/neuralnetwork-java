280b898c5bf0b36ee8468c253116af3f88ca9910
matrentman <mikeatrentman@yahoo.com>
2022-10-14 15:36:47 -0400

    Initial commit with basic perceptron example

---
+++ src/main/java/org/eMpTy/App.java
@@ -0,0 +1,32 @@
+package org.eMpTy;
+
+/**
+ * App
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // inputs
+        double[] x = {0, 1};
+        // weights
+        double[] w = {0.5, 0.5};
+        // bias
+        double b = 0.5;
+        // weighted sum
+        double z = 0.0;
+
+        // calculate the weighted sum
+        for (int i = 0; i < x.length; i++) {
+            z += x[i] * w[i];
+        }
+        z += b;
+
+        // activation function
+        double a = z > 0 ? 1.0 : 0.0;
+
+        // print activation output
+        System.out.println(a);
+    }
+}
