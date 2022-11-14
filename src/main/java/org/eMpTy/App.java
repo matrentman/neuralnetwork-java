78f9548a966063f44f5d7333c9cfb9396a5ff7ef
matrentman <mikeatrentman@yahoo.com>
2022-11-14 14:23:15 -0500

    Logic gates, added AND gate

--- src/main/java/org/eMpTy/App.java
+++ src/main/java/org/eMpTy/App.java
@@ -1,32 +1,40 @@
 package org.eMpTy;
 
 /**
- * App
+ * Logic Gates
  *
+ * Input   AND   OR   XOR   NOR   NAND   XNOR
+ * 00      0     0    0     1     1      1
+ * 01      0     1    1     0     1      0
+ * 10      0     1    1     0     1      0
+ * 11      1     1    0     0     0      1
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        // inputs
-        double[] x = {0, 1};
-        // weights
-        double[] w = {0.5, 0.5};
-        // bias
-        double b = 0.5;
-        // weighted sum
+    static double neuron(double[] x, double[] w, double b) {
         double z = 0.0;
 
-        // calculate the weighted sum
         for (int i = 0; i < x.length; i++) {
             z += x[i] * w[i];
         }
         z += b;
 
-        // activation function
-        double a = z > 0 ? 1.0 : 0.0;
+        return z > 0 ? 1.0 : 0.0;
+    }
 
-        // print activation output
-        System.out.println(a);
+    static double and(double x1, double x2) {
+        return neuron(new double[] {x1, x2}, new double[] {1, 1}, -1);
+    }
+
+    public static void main( String[] args )
+    {
+        for (int i=0; i<4; i++) {
+            double x1 = i/2;
+            double x2 = i%2;
+
+            double output = and(x1, x2);
+
+            System.out.printf("%d%d\t%d\n", (int)x1, (int)x2, (int)output);
+        }
     }
 }
